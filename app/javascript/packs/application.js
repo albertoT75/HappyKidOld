b6c02dce21417fec2b57d641230bebfdb1f6a1ff
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-04-15 12:04:20 +0200

    configuring all the app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,87 @@
 import "bootstrap";
+import "jquery";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../plugins/init_mapbox';
+import "select2";
+import "select2/dist/css/select2.css";
+
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
+
+var scrollWindow = function() {
+  $(window).scroll(function(){
+    var $w = $(this),
+        st = $w.scrollTop(),
+        navbar = $('.ftco_navbar'),
+        sd = $('.js-scroll-wrap');
+
+    if (st > 150) {
+      if ( !navbar.hasClass('scrolled') ) {
+        navbar.addClass('scrolled');
+      }
+    }
+    if (st < 150) {
+      if ( navbar.hasClass('scrolled') ) {
+        navbar.removeClass('scrolled sleep');
+      }
+    }
+    if ( st > 350 ) {
+      if ( !navbar.hasClass('awake') ) {
+        navbar.addClass('awake');
+      }
+
+      if(sd.length > 0) {
+        sd.addClass('sleep');
+      }
+    }
+    if ( st < 350 ) {
+      if ( navbar.hasClass('awake') ) {
+        navbar.removeClass('awake');
+        navbar.addClass('sleep');
+      }
+      if(sd.length > 0) {
+        sd.removeClass('sleep');
+      }
+    }
+  });
+};
+scrollWindow();
+var OnePageNav = function() {
+  $(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
+     e.preventDefault();
+
+     var hash = this.hash,
+         navToggler = $('.navbar-toggler');
+     $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 700, 'easeInOutExpo', function(){
+      window.location.hash = hash;
+    });
+    if ( navToggler.is(':visible') ) {
+      navToggler.click();
+    }
+  });
+  $('body').on('activate.bs.scrollspy', function () {
+    console.log('nice');
+  })
+};
+OnePageNav();
+initMapbox();
+
+
+document.querySelectorAll(".advanced_search").forEach((inputField) => {
+  $(inputField).select2({width: "140px"});
+})
+
+$(".advanced_search").select2({
+    placeholder: "Select an option"
+});
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :kids
-
-
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 
 end
