a2ed2b88cee6d475b59d25de3863bf6c9fcce95d
Alberto Tarantino <albertotarantino75@yahoo.it>
2020-04-19 16:26:01 +0200

    model fixed

--- db/migrate/20200417105658_foreign_keys_game.rb
+++
@@ -1,7 +0,0 @@
-class ForeignKeysGame < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :games, :area, foreign_key: true
-    add_reference :games, :customize, foreign_key: true
-    add_reference :games, :reward, foreign_key: true
-  end
-end
